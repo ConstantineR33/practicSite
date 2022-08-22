8b2b455d28b76c9f8de40bdbe9331acc38e622a0
Constantine <93536536+ConstantineR33@users.noreply.github.com>
2022-08-22 21:15:36 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeScrean from './Components/HomeScrean/HomeScrean';
+
+function App() {
+  return (
+    <div className="App">
+   <HomeScrean/>
+    </div>
+  );
+}
+
+export default App;
